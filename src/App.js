6f01519798430dbde1a6f474ae2c6db457a188df
vitek999 <vitek2012rus@gmail.com>
2023-04-01 13:47:32 +0300

    feat: voice select

--- src/App.js
+++ src/App.js
@@ -1,58 +1,78 @@
 import './App.css';
-import { TextField, Button } from '@mui/material';
-import { useEffect, useState, useRef } from 'react';
+import {TextField, Button, Select, SelectChangeEvent, MenuItem} from '@mui/material';
+import {useEffect, useState, useRef} from 'react';
 
 function App() {
-  const [text, setText] = useState('')
-  const [files, setFiles] = useState([])
-  const audioRef = useRef(null);
+    const [text, setText] = useState('')
+    const [files, setFiles] = useState([])
+    const audioRef = useRef(null);
+    const [voice, setVoice] = useState(0)
 
-  const handlePlay = (fileName) => {
-    console.log(fileName)
+    const handlePlay = (fileName) => {
+        console.log(fileName)
 
-    if (audioRef.current != null) {
-      audioRef.current.pause()
+        if (audioRef.current != null) {
+            audioRef.current.pause()
+        }
+        audioRef.current = new Audio(`http://127.0.0.1:8000/audio_file?filename=${fileName}`)
+        audioRef.current.play()
     }
-    audioRef.current = new Audio(`http://127.0.0.1:8000/audio_file?filename=${fileName}`)
-    audioRef.current.play()
-  }
 
-  const handleClick = async (text) => {
-    await fetch(`http://127.0.0.1:8000/tinkoff-text-to-speech?text=${text}`)
-    await fetchFiles()
-  }
+    const handleClick = async (text, voice_index) => {
+        await fetch(`http://127.0.0.1:8000/tinkoff-text-to-speech?text=${text}&voice_index=${voice_index}`)
+        await fetchFiles()
+    }
 
-  const fetchFiles = async () => {
-    const response = await fetch(`http://127.0.0.1:8000/generated_audio`)
-    const decoded = await response.json()
-    console.log(decoded)
-    setFiles(decoded)
-  }
+    const handleVoiceChange = (event) => {
+        setVoice(event.target.value);
+    }
 
-  useEffect(() => {
-    fetchFiles()
-  }, [])
+    const fetchFiles = async () => {
+        const response = await fetch(`http://127.0.0.1:8000/generated_audio`)
+        const decoded = await response.json()
+        console.log(decoded)
+        setFiles(decoded)
+    }
 
-  return (
-    <div className="App">
-      <TextField id="outlined-multiline-flexible" label="Multiline" multiline onChange={(event) => { setText(event.target.value) }} />
-      <Button variant="outlined" onClick={() => { handleClick(text) }}>Создать</Button>
-      <FilesList files={files} handlePlay={handlePlay} />
-    </div>
-  );
+    useEffect(() => {
+        fetchFiles()
+    }, [])
+
+    return (
+        <div className="App">
+            <TextField id="outlined-multiline-flexible" label="Multiline" multiline onChange={(event) => {
+                setText(event.target.value)
+            }}/>
+            <Select
+                value={voice}
+                onChange={handleVoiceChange}
+                label='Voice'
+            >
+                <MenuItem value={0}>Алёна</MenuItem>
+                <MenuItem value={1}>Максим</MenuItem>
+                <MenuItem value={2}>Флирт</MenuItem>
+            </Select>
+            <Button variant="outlined" onClick={() => {
+                handleClick(text, voice)
+            }}>Создать</Button>
+            <FilesList files={files} handlePlay={handlePlay}/>
+        </div>
+    );
 }
 
 const FilesList = (props) => {
-  const filesItems = props.files.map((file, index) =>
-    <li key={index}>
-      {file}
-      <Button onClick={() => { props.handlePlay(file) }}>
-        play
-      </Button>
-    </li>);
-  return (
-    <ul>{filesItems}</ul>
-  );
+    const filesItems = props.files.map((file, index) =>
+        <li key={index}>
+            {file}
+            <Button onClick={() => {
+                props.handlePlay(file)
+            }}>
+                play
+            </Button>
+        </li>);
+    return (
+        <ul>{filesItems}</ul>
+    );
 }
 
 export default App;
